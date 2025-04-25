15e06e400943bb47e07f38b23ab950c838c1cd33
muhcrz <cruzmurilohenrique@gmail.com>
2025-04-25 14:53:54 -0300

    commit pronto

--- Datasigno.java
+++ Datasigno.java
@@ -27,15 +27,30 @@ public class Datasigno {
     }else if ((dia >= 21 && mês == 5) || (dia <= 20 && mês == 6)) {
         signo = "Gêmeos";
     }else if ((dia >= 22 && mês == 6) || (dia <= 22 && mês == 7)) {
+        signo = "Câncer";
     }else if ((dia >= 23 && mês == 7) || (dia <= 22 && mês == 8)) {
+        signo = "Leão";
     }else if ((dia >=23  && mês == 8) || (dia <= 22 && mês == 9)) {
+        signo = "Virgem";
     }else if ((dia >=23  && mês == 9) || (dia <= 22 && mês == 10)) {
+        signo = "Libra";
     }else if ((dia >=23  && mês == 10) || (dia <= 21 && mês == 11)) {
-    }else if ((dia >=22  && mês == 11) || (dia <= 21 && mês == 12)) {   
+        signo = "Escorpião";
+    }else if ((dia >=22  && mês == 11) || (dia <= 21 && mês == 12)) {
+        signo = "Sagitário";   
     }else if ((dia >=22  && mês == 12) || (dia <= 19 && mês == 1)) {
+        signo = "Capricórnio";
     }else if ((dia >=20  && mês == 1) || (dia <= 18 && mês == 2)) {
+        signo = "Aquário";
     }else if ((dia >=19  && mês == 2) || (dia <= 20 && mês == 3)) {
-   
-   
+        signo = "Peixes";
+    } else {
+        signo = "Inválido (data incorreta)";
     }
-    }}
+   
+   
+        //Saída 
+    System.out.println("Seu Signo é " + signo);
+
+}}
+
